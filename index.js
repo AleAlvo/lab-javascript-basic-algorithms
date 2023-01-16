0bee2ba42e0f9f4e1f8082de0f6a94618e014b5f
Alexandre Alvaro <a.borges.alvaro@gmail.com>
2023-01-16 17:53:23 +0000

    add Iteration 1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Iteration 1: Names and Input
-
-
+console.log("I'm ready")
+let hacker1 = "Alex";
+console.log(`The driver's name is ${hacker1}`)
+let hacker2 = "Mike";
+console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
